75e01c07d7d9410aff8eea18d3fedb61b5ef2b65
Ian Darland <ianpdarland@gmail.com>
2023-05-01 11:26:16 -0500

    updated navigation working seamless

--- pages/Pokedex/index.js
+++ pages/Pokedex/index.js
@@ -2,13 +2,13 @@ import React, { useState } from "react";
 import { Button, Image, ScrollView, View } from "react-native";
 import PokeContainer from "../../components/PokeContainer";
 
-const Pokedex = () => {
+const Pokedex = ({ navigation }) => {
     const [pixState, setPixState] = useState(false)
 
     return(
         <>
         <View>
-            <PokeContainer/>
+            <PokeContainer navigation={navigation}/>
         </View>
         </>
     )
